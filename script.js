06572a8c5106adff9d0a0b6936ea826377f3627c
Jelbro <Jelbro97@gmail.com>
2024-09-30 12:34:42 +0100

    Add comments to functions

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+/**
+ *A function that generates a random number between 0-2 and
+ *returns either rock, paper or scissor depending on the number.
+ * @return {string}
+ */
 function getComputerChoice() {
 	const random_number = Math.floor(Math.random() * 3);
 	if (random_number == 0) {
@@ -10,6 +15,11 @@ function getComputerChoice() {
 	}
 }
 
+/**
+ *A function that prompts the user for a choice between rock,
+ *paper or scissors and returns the choice. Will loop if incorrect input given.
+ * @return {string}
+ */
 function getHumanChoice() {
 	while (true) {
 		const human_choice = prompt('Please enter (R)ock, (P)aper or (S)cissors!');
