0abd5e4c52ec637bcf9f877e3a96ae577887403e
Jelbro <Jelbro97@gmail.com>
2024-09-30 11:15:50 +0100

    Complete getComputerChoice()

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 function getComputerChoice() {
-	return computer_choice;
+	const random_number = Math.floor(Math.random() * 3);
+	if (random_number == 0) {
+		return 'rock';
+	} else if (random_number == 1) {
+		return 'paper';
+	} else if (random_number == 2) {
+		return 'scissors';
+	} else {
+	}
 }
